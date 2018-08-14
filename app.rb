28f91ed2218c34ba82f7d0f65f03c06bcefeb241
Litzy Bucio <lbucio2@cps.edu>
2018-08-14 19:06:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+  "Henery!"
+endget '/' do
+  "Hello, World!"
+endget '/' do
+  "Hello, World!"
+endget '/' do
+  "Hello, World!"
+end
 end
