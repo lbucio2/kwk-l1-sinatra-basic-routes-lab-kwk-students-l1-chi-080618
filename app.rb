f53b12311f5eeb6e437a0f64f2fcc9d5c26a8143
Litzy Bucio <lbucio2@cps.edu>
2018-08-14 19:07:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,14 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
-  get '/name' do
-  "Henery!"
-endget '/' do
-  "Hello, World!"
-endget '/' do
-  "Hello, World!"
-endget '/' do
-  "Hello, World!"
-end
+
+  get '/name' do 
+    "My name is Mimi"
+  end
+
+  get '/hometown' do 
+    "My hometown is Western Springs"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Oh, Jeez"
+  end
 end
